67b2afd3d1d19a65426d339efd23784232119420
[Cenk Ergüven] <[erguven.cenk@gmail.com]>
2022-07-27 00:36:49 +0300

    ArmstrongSayi programı eklendi.

---
+++ src/ArmstrongSayi.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class ArmstrongSayi {
+    public static void main(String[] args) {
+        System.out.println("Enter a number : ");
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int total = 0;
+        int remain;
+
+        while ( a>0 ){
+            remain = a % 10;
+            total += remain;
+            a = a /10;
+
+        }
+        System.out.println("Sum of the digits of the number is " + total );
+
+
+
+
+
+    }
+}
