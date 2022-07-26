480abb3eeea54a65cc0149d0e1a992bf795300a4
[Cenk Ergüven] <[erguven.cenk@gmail.com]>
2022-07-26 19:38:32 +0300

    Kombinasyon commiti eklendi.

---
+++ src/Kombinasyon.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Kombinasyon {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int n, r, fn = 1, fr = 1, fm = 1, combination;
+
+        /*
+        fn,n nin faktoriyeli;
+        fr,r nin faktoriyeli;
+        fm ise n-r nin faktoriyelidir.
+         */
+
+        System.out.println("The r combination of N is denoted as C(n,r).\nPlase enter the N:");
+        n = inp.nextInt();
+        System.out.println("Please enter the r:");
+        r = inp.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            fn = fn * i;
+        }
+        for (int k = 1; k <= r; k++) {
+            fr = fr * k;
+        }
+        int minus = n - r;
+        for (int m = 1; m <= minus; m++) {
+            fm = fm * m;
+        }
+        combination = fn / (fr * fm);
+        System.out.println("The combination is:" + combination);
+    }
+
+}
